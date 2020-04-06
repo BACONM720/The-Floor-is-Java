85b2a04db1b6a725a6b082c64586cd39c8aa537a
seanh2000 <47183165+seanh2000@users.noreply.github.com>
2020-04-06 14:01:01 +0100

    Create mealsFunctions.js
    Button click brings us to new page with filters applied

---
+++ public/javascripts/mealsFunctions.js
@@ -0,0 +1,63 @@
+$(document).ready(
+function() {
+	$("#mealsButton").click(function () {
+		var search = "http://danu7.it.nuigalway.ie:8618/getMealsData"; // CHANGE PORT
+		var filterCount = 0;
+		if ($("#vegan").is(":checked")) {
+			if(filterCount>0) {
+				search+="&vegan=true";
+			}
+			else {
+				search+= "?vegan=true";
+			}
+			filterCount+=1;
+		}
+		if ($("#gluten-free").is(":checked")) {
+			if(filterCount>0) {
+				search+="&glutfree=true";
+			}
+			else {
+				search+= "?glutfree=true";
+			}
+			filterCount+=1;
+		}
+		if ($("#dairy-free").is(":checked")) {
+			if(filterCount>0) {
+				search+="&dFree=true";
+			}
+			else {
+				search+= "?dFree=true";
+			}
+			filterCount+=1;
+		}
+		if ($("#high-calorie").is(":checked")) {
+			if(filterCount>0) {
+				search+="&hCalorie=true";
+			}
+			else {
+				search+= "?hCalorie=true";
+			}
+			filterCount+=1;
+		}
+		if ($("#low-calorie").is(":checked")) {
+			if(filterCount>0) {
+				search+="&lCalorie=true";
+			}
+			else {
+				search+= "?lCalorie=true";
+			}
+			filterCount+=1;
+		}
+		if ($("#vegetarian").is(":checked")) {
+			if(filterCount>0) {
+				search+="&veg=true";
+			}
+			else {
+				search+= "?veg=true";
+			}
+			filterCount+=1;
+		}
+		window.location.href = search;
+	});
+}
+);
